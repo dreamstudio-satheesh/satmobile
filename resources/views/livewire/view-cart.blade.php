27df59fc74712879db3163a142c81e31928c0a60
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-27 15:27:11 +0530

    app

--- resources/views/livewire/view-cart.blade.php
+++ resources/views/livewire/view-cart.blade.php
@@ -5,64 +5,23 @@
             <div class="table-responsive card-body">
                 <table class="table mb-0">
                     <tbody>
-                        <tr>
-                            <th scope="row"><a class="remove-product" href="#"><i
-                                        class="fa-solid fa-xmark"></i></a></th>
-                            <td><img class="rounded" src="img/product/11.png" alt=""></td>
-                            <td><a href="single-product.html">Ciramic Pot Set<span>$12.10 × 1</span></a></td>
-                            <td>
-                                <div class="quantity">
-                                    <input class="qty-text" type="text" value="1">
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><a class="remove-product" href="#"><i
-                                        class="fa-solid fa-xmark"></i></a></th>
-                            <td><img class="rounded" src="img/product/14.png" alt=""></td>
-                            <td><a href="single-product.html">Bluetooth Speaker<span>$9.87 × 2</span></a></td>
-                            <td>
-                                <div class="quantity">
-                                    <input class="qty-text" type="text" value="2">
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row"><a class="remove-product" href="#"><i
-                                        class="fa-solid fa-xmark"></i></a></th>
-                            <td><img class="rounded" src="img/product/10.png" alt=""></td>
-                            <td><a href="single-product.html">Modern Gray Tops<span>$7 × 1</span></a></td>
-                            <td>
-                                <div class="quantity">
-                                    <input class="qty-text" type="text" value="1">
-                                </div>
-                            </td>
-                        </tr>
+                        @foreach($cart as $productId => $productDetails)
+                            <tr>
+                                <td>{{ $productDetails['name'] }}</td>
+                                <td>{{ $productDetails['price'] }}</td>
+                                <td>
+                                    <input type="number" min="1" value="{{ $productDetails['quantity'] }}" wire:change="updateQuantity({{ $productId }}, $event.target.value)">
+                                </td>
+                                <td>{{ $productDetails['price'] * $productDetails['quantity'] }}</td>
+                                <td>
+                                    <button wire:click="removeFromCart({{ $productId }})">Remove</button>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
-        <!-- Coupon Area-->
-        <div class="card coupon-card mb-3">
-            <div class="card-body">
-                <div class="apply-coupon">
-                    <h6 class="mb-0">Have a coupon?</h6>
-                    <p class="mb-2">Enter your coupon code here &amp; get awesome discounts!</p>
-                    <div class="coupon-form">
-                        <form action="#">
-                            <input class="form-control" type="text" placeholder="SUHA30">
-                            <button class="btn btn-primary" type="submit">Apply</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Cart Amount Area-->
-        <div class="card cart-amount-area">
-            <div class="card-body d-flex align-items-center justify-content-between">
-                <h5 class="total-price mb-0">$<span class="counter">38.84</span></h5><a class="btn btn-warning"
-                    href="checkout.html">Checkout Now</a>
-            </div>
-        </div>
+     
     </div>
 </div>
