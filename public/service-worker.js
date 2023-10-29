f0c8e7a5f3b7e56b36356f67380231a9d3f796bf
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-29 08:59:10 +0530

    app

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,50 +1,24 @@
-// -----------------------------------------------------------------
-// Template Name: Suha - Multipurpose Ecommerce Mobile HTML Template
-// Template Author: Designing World
-// Template Author URL: https://themeforest.net/user/designing-world
-// -----------------------------------------------------------------
-
 const staticCacheName = 'precache-v3.0.1';
 const dynamicCacheName = 'runtimecache-v3.0.1';
 
 // Pre Caching Assets
 const precacheAssets = [
     '/',
-    'img/core-img/logo-small.png',
-    'img/core-img/logo-white.png',
-    'img/bg-img/no-internet.png',
-    'js/theme-switching.js',
-    'offline.html'
+    '/img/core-img/logo-small.png',
+    '/img/core-img/logo-white.png',
+    '/img/bg-img/no-internet.png',
+    '/js/theme-switching.js',
+    '/offline.html'
 ];
 
 // Install Event
-
 self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return Promise.all(precacheAssets.map(function (url) {
-                return fetch(url)
-                    .then(function (response) {
-                        if (!response.ok) {
-                            throw new Error('Failed to fetch: ' + url);
-                        }
-                        return cache.put(url, response);
-                    })
-                    .catch(function (error) {
-                        console.error('Failed to fetch resource:', error);
-                    });
-            }));
-        })
-    );
-});
-
-/* self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(staticCacheName).then(function (cache) {
             return cache.addAll(precacheAssets);
         })
     );
-}); */
+});
 
 // Activate Event
 self.addEventListener('activate', function (event) {
@@ -60,35 +34,23 @@ self.addEventListener('activate', function (event) {
 
 // Fetch Event
 self.addEventListener('fetch', function (event) {
-    if (event.request.method === 'GET') {
-        event.respondWith(
-            caches.match(event.request).then(cacheRes => {
-                return cacheRes || fetch(event.request).then(response => {
-                    return caches.open(dynamicCacheName).then(function (cache) {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    })
-                });
-            }).catch(function () {
-                // Fallback Page, When No Internet Connection
-                return caches.match('offline.html');
-            })
-        );
-    }
-});
-
-/* self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request).then(cacheRes => {
             return cacheRes || fetch(event.request).then(response => {
-                return caches.open(dynamicCacheName).then(function (cache) {
-                    cache.put(event.request, response.clone());
+                // Check if the response is valid before caching
+                if (response && response.status === 200) {
+                    return caches.open(dynamicCacheName).then(function (cache) {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    });
+                } else {
+                    // Handle non-200 responses here
                     return response;
-                })
+                }
+            }).catch(function() {
+                // Fallback Page, When No Internet Connection
+                return caches.match('/offline.html');
             });
-        }).catch(function() {
-            // Fallback Page, When No Internet Connection
-            return caches.match('offline.html');
-          })
+        })
     );
-}); */
+});
