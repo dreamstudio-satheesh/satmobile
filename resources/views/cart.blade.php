d55481533e588886825f060e9a166cb6b26d5a83
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-29 10:54:25 +0530

    app

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -26,37 +26,38 @@
     </div>
 
 
-<div class="container">
-    <!-- Cart Wrapper-->
-    <div class="cart-wrapper-area py-3">
-        <div class="cart-table card mb-3">
-            <div class="table-responsive card-body">
-                <table class="table mb-0">
-                    <thead>
-                        <tr>
-                            <th>Product</th>
-                            <th>Price</th>
-                            <th>Quantity</th>
-                            <th>Total</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody id="cart-body">
-                        <!-- Cart items will be dynamically added here using JavaScript -->
-                    </tbody>
-                </table>
+    <div class="container">
+        <!-- Cart Wrapper-->
+        <div class="cart-wrapper-area py-3">
+            <div class="cart-table card mb-3">
+                <div class="table-responsive card-body">
+                    <table class="table mb-0">
+                        <thead>
+                            <tr>
+                                <th>Product</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Total</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody id="cart-body">
+                            <!-- Cart items will be dynamically added here using JavaScript -->
+                        </tbody>
+                    </table>
 
-                <script>
-                    var cartData = @json($cart);
-                    // Function to render the cart items
-                    function renderCartItems() {
-                        var cartBody = document.getElementById('cart-body');
-                        cartBody.innerHTML = '';
+                    <script>
+                        // Retrieve cart data from localStorage
+                        var cartData = JSON.parse(localStorage.getItem('cart'));
+                        // Function to render the cart items
+                        function renderCartItems() {
+                            var cartBody = document.getElementById('cart-body');
+                            cartBody.innerHTML = '';
 
-                        for (var productId in cartData) {
-                            var productDetails = cartData[productId];
-                            var row = document.createElement('tr');
-                            row.innerHTML = `
+                            for (var productId in cartData) {
+                                var productDetails = cartData[productId];
+                                var row = document.createElement('tr');
+                                row.innerHTML = `
                                 <td>${productDetails.name}</td>
                                 <td>${productDetails.price}</td>
                                 <td>
@@ -68,40 +69,37 @@
                                 </td>
                             `;
 
-                            cartBody.appendChild(row);
+                                cartBody.appendChild(row);
+                            }
                         }
-                    }
 
-                    // Function to update quantity
-                    function updateQuantity(productId, newQuantity) {
-                        // Update the cartData object
-                        cartData[productId].quantity = parseInt(newQuantity);
-                        // Call your backend to update the cart data as well
+                        // Function to update quantity
+                        function updateQuantity(productId, newQuantity) {
+                            // Update the cartData object
+                            cartData[productId].quantity = parseInt(newQuantity);
+                            // Call your backend to update the cart data as well
 
-                        // Re-render the cart items
+                            // Re-render the cart items
+                            renderCartItems();
+                        }
+
+                        // Function to remove a product from the cart
+                        function removeFromCart(productId) {
+                            // Remove the product from cartData
+                            delete cartData[productId];
+                            // Call your backend to update the cart data as well
+
+                            // Re-render the cart items
+                            renderCartItems();
+                        }
+
+                        // Initial rendering of cart items
                         renderCartItems();
-                    }
+                    </script>
 
-                    // Function to remove a product from the cart
-                    function removeFromCart(productId) {
-                        // Remove the product from cartData
-                        delete cartData[productId];
-                        // Call your backend to update the cart data as well
-
-                        // Re-render the cart items
-                        renderCartItems();
-                    }
-
-                    // Initial rendering of cart items
-                    renderCartItems();
-                </script>
-
+                </div>
             </div>
+
         </div>
-
     </div>
-</div>
-
-
-
 @endsection
