9c025f4a06ecbb9bfbbe443f88d5c6e789249104
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-29 08:38:01 +0530

    app

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'login', // Add the name or URI of your login route here
     ];
 }
