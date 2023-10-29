11e54fda015c33842697a1a77198d538c48ecf48
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-29 18:44:50 +0530

    app

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -3,28 +3,12 @@
 @section('content')
     <!-- Search Form-->
     <div class="container">
-        <div class="search-form pt-3 rtl-flex-d-row-r">
-            <select class="form-select form-select-sm" aria-label=".form-select-sm example">
-                <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            <!-- Alternative Search Options -->
-            <div class="alternative-search-options">
-                <div class="dropdown"><a class="btn btn-danger dropdown-toggle" id="altSearchOption" href="#"
-                        role="button" data-bs-toggle="dropdown" aria-expanded="false"><i
-                            class="fa-solid fa-sliders"></i></a>
-                    <!-- Dropdown Menu -->
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="altSearchOption">
-                        <li><a class="dropdown-item" href="#"><i class="fa-solid fa-microphone"> </i>Voice
-                                Search</a></li>
-                        <li><a class="dropdown-item" href="#"><i class="fa-solid fa-image"> </i>Image
-                                Search</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
     </div>
 
 
