718da018f6856d93d499f0ef73b55600aa0ed2a7
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-30 10:10:50 +0530

    app

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,6 @@
 const staticCacheName = 'precache-v3.0.1';
 const dynamicCacheName = 'runtimecache-v3.0.1';
+const productImageCacheName = 'product-images'; // Define the cache name for product images
 
 // Pre Caching Assets
 const precacheAssets = [
@@ -33,7 +34,6 @@ self.addEventListener('install', function (event) {
     );
 });
 
-
 // Activate Event
 self.addEventListener('activate', function (event) {
     event.waitUntil(
@@ -52,6 +52,30 @@ self.addEventListener('fetch', function (event) {
     if (event.request.method === 'POST') {
         // Do not cache POST requests, simply return the fetch response
         event.respondWith(fetch(event.request));
+        return;
+    }
+
+    // Check if the request is for product images and serve from the product image cache
+    if (event.request.url.startsWith('https://satsweets.com/storage/')) {
+        event.respondWith(
+            caches.match(event.request).then(cacheRes => {
+                return cacheRes || fetch(event.request).then(response => {
+                    // Cache product images in the product image cache
+                    if (response && response.status === 200) {
+                        return caches.open(productImageCacheName).then(function (cache) {
+                            cache.put(event.request, response.clone());
+                            return response;
+                        });
+                    } else {
+                        // Handle non-200 responses here for product images
+                        return response;
+                    }
+                }).catch(function() {
+                    // Fallback for product images when no internet connection
+                    return caches.match('https://satsweets.com/assets/img/product/product29.jpg');
+                });
+            })
+        );
         return;
     }
 
