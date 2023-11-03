35184bf4ae4c51aa21de692358346dbdc026af03
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-11-03 09:31:11 +0530

    app

--- routes/web.php
+++ routes/web.php
@@ -48,4 +48,4 @@ Route::get('/cart.html', [App\Http\Controllers\HomeController::class, 'cart'])->
 
 // routes/web.php
 
-Route::post('/checkout',  [App\Http\Controllers\CheckoutController::class, 'cart'])->name('checkout');
+Route::post('/checkout',  [App\Http\Controllers\CheckoutController::class, 'checkout'])->name('checkout');
