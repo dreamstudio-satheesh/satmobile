e479c6bc35c8a2534bcb846ca83ac2113622c38d
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-11-03 12:49:11 +0530

    app

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -42,62 +42,71 @@ class CheckoutController extends Controller
             $totalAmount += $itemTotal; // Accumulate the total amount
         }
 
-     
         if (!empty($cartlist) && !empty($customerId) && !empty($invoice_date)) {
-         
-            $invoiceId =  $this->create_invoice([
-                   'items' =>  $cartlist,
-                   'customer_id' => $customerId,
-                   'invoice_date' => $invoice_date,
-                   'total' => $totalAmount,
-                   'sub_total' => $totalAmount - $tax_amount,
-                   'taxamount' => $tax_amount,
-                   
-               ]); 
-           }
+            $invoiceId = $this->create_invoice([
+                'items' => $cartlist,
+                'customer_id' => $customerId,
+                'invoice_date' => $invoice_date,
+                'total' => $totalAmount,
+                'sub_total' => $totalAmount - $tax_amount,
+                'taxamount' => $tax_amount,
+            ]);
+        }
 
-           if (!$invoiceId) {
+        if (!$invoiceId) {
             return response()->json(['message' => 'Failed to create invoice.']);
         }
 
         $response = [
-            'message' => 'Invoice created successful'
+            'message' => 'Invoice created successful',
         ];
-
 
         return response()->json($response);
     }
 
-
     public function create_invoice($data)
     {
-        $invoice_num=Invoice::withTrashed()->max('invoice_number')+1;        
+        try {
+            // Start a new database transaction
+            DB::beginTransaction();
 
-        $invoice=Invoice::create([
-            'invoice_number' => $invoice_num,
-            'date' => $data['invoice_date'],
-            'customer_id' => $data['customer_id'],
-            'taxamount' => $data['taxamount'],
-            'sub_total' => $data['sub_total'],
-            'total' => $data['total'],
-            'created_by' => Auth::user()->id
-        ]);
-        //dd($invoice);
+            $invoice_num = Invoice::withTrashed()->max('invoice_number') + 1;
 
-        foreach ($data['items'] as $key => $item) {
-            Invoice_item::create([
-            'invoice_id' => $invoice->id,
-            'product_code' => $item['code'],
-            'name' => $item['name'],
-            'price' => $item['price'],
-            'gst' => $item['gst'],
-            'hsncode' => $item['hsncode'],
-            'gstamount' => $item['gstamount'],
-            'quantity' => $item['quantity'],
-            'total' => $item['total'],
-          ]);
-          
+            $invoice = Invoice::create([
+                'invoice_number' => $invoice_num,
+                'date' => $data['invoice_date'],
+                'customer_id' => $data['customer_id'],
+                'taxamount' => $data['taxamount'],
+                'sub_total' => $data['sub_total'],
+                'total' => $data['total'],
+                'created_by' => Auth::user()->id,
+            ]);
+
+            foreach ($data['items'] as $key => $item) {
+                Invoice_item::create([
+                    'invoice_id' => $invoice->id,
+                    'product_code' => $item['code'],
+                    'name' => $item['name'],
+                    'price' => $item['price'],
+                    'gst' => $item['gst'],
+                    'hsncode' => $item['hsncode'],
+                    'gstamount' => $item['gstamount'],
+                    'quantity' => $item['quantity'],
+                    'total' => $item['total'],
+                ]);
+            }
+
+            // Commit the transaction
+            DB::commit();
+            return $invoice->id;
+        } catch (\Exception $e) {
+            // Rollback the transaction in case of errors
+            DB::rollback();
+
+            // Log the error for debugging
+            \Log::error('Error creating invoice: ' . $e->getMessage());
+
+            return false;
         }
-         
     }
 }
