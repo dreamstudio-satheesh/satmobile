1dc25867a9b9eb01034b5b4d240925a4865d6ee9
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-11-03 12:53:25 +0530

    app

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Invoice;
 use App\Models\Product;
+use App\Models\Invoice_item;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
