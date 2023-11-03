513d6c98d9d41824f53c89236110b8bd8fe0e661
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-11-03 11:21:14 +0530

    app

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -231,7 +231,7 @@
             var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
             var yyyy = today.getFullYear();
 
-            today = yyyy + '-' + mm + '-' + dd;
+            today = dd + '-' + mm + '-' + yyyy;
             document.getElementById('dateInput').value = today;
 
 
